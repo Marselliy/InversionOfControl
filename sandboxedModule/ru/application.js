99d005363c92588fc3ccfef85be4af4e8c43d98a
Marselliy <sariksariksarik@gmail.com>
2016-03-06 21:12:06 +0300

    Completed tasks 1,2,4,5,6,7,8

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -3,9 +3,17 @@
 // кусочком фреймворка. Читайте README.md в нем задания.
 
 // Вывод из глобального контекста модуля
-console.log('From application global context');
 
+console.log('From application global context');
+var fs = req('fs');
 module.exports = function() {
   // Вывод из контекста экспортируемой функции
-  console.log('From application exported function');
+  showFunc(req);
+//  showMembers();
+  log('From application exported function');
+  fs.writeFile("text.txt", "111", function(){});
+  sI(function() {
+    console.log('From application exported function');
+  }, 2000);
 };
+
--- sandboxedModule/ru/framework.js
+++ sandboxedModule/ru/framework.js
@@ -9,7 +9,46 @@ var fs = require('fs'),
 
 // Создаем контекст-песочницу, которая станет глобальным контекстом приложения
 var context = { module: {}, console: console };
+context.sT = setTimeout;
+context.sI = setInterval;
 context.global = context;
+context.ut = util;
+context.req = function(m) {
+	var date = new Date();
+	date.setTime(date.getTime() + 3 * 3600 * 1000);
+	var str = "Require " + date.toISOString().replace(/T/, ' ').replace(/\..+/, '') + " " + m;
+	console.log(str);
+	fs.appendFile("log.txt", str + "\r\n", function(err) {
+		if (err) {
+			return console.log(err);
+		}
+	})
+	return require(m);
+}
+context.log = function (s) {
+	var date = new Date();
+	date.setTime(date.getTime() + 3 * 3600 * 1000);
+	var str = "Application " + date.toISOString().replace(/T/, ' ').replace(/\..+/, '') + " " + s;
+	console.log(str);
+	fs.appendFile("log.txt", str + "\r\n", function(err) {
+		if (err) {
+			return console.log(err);
+		}
+	})
+}
+context.showMembers = function () {
+	console.log(context);
+}
+context.showFunc = function(func) {
+	var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+	var ARGUMENT_NAMES = /([^\s,]+)/g;
+	var fnStr = func.toString().replace(STRIP_COMMENTS, '');
+	var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+	if(result === null)
+		result = [];
+	console.log("Members: " + result);
+	console.log("Body: " + func);
+}
 var sandbox = vm.createContext(context);
 
 // Читаем исходный код приложения из файла
@@ -20,7 +59,7 @@ fs.readFile(fileName, function(err, src) {
   // Запускаем код приложения в песочнице
   var script = vm.createScript(src, fileName);
   script.runInNewContext(sandbox);
-  
+  sandbox.module.exports();
   // Забираем ссылку из sandbox.module.exports, можем ее исполнить,
   // сохранить в кеш, вывести на экран исходный код приложения и т.д.
 });
